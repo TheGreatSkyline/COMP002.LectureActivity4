1d6b65168107bcd598d1a981311c6e711765f5b2
Joaquin Gomez <joaquingomez@my.whccd.edu>
2024-11-06 23:38:26 -0800

    console figure

--- js/script.js
+++ js/script.js
@@ -5,3 +5,4 @@
 *    Purpose: JavaScript for Lecture Activity 4
 */
 let figures = [25, 35, 36, 40, 42];//Array in JavaScript are zero index, meaning the first element is at index 0.
+console.log(figures[0]);// figures[0] accesses the first element of the array, which is 25 in this case.
