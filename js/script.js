3f23af6fc2e027555adbaa790a29dca14af0da3f
Joaquin Gomez <joaquingomez@my.whccd.edu>
2024-11-06 23:54:14 -0800

    for let

--- js/script.js
+++ js/script.js
@@ -10,3 +10,8 @@ console.log(figures[figures.length - 2]);// To get the second to last element of
 // Figure.length returns the number of elements in the array, which is 5 in this case.
 // Since the array is zero-indexed, the last element is at index length - 1, giving us 42
 let newFigures = [25, 35, 36, 40, 42];
+for (let i = 0; i < newFigures.length; i++) {// let i = 0: Initalizes the loop counter i to 0, which is the starting point of the index of the array.        
+// i < newFigures.length: Continues the loop as long as i is less than the length of the array.
+// i++: Increments the loop counter i by 1 after each iteration of the loop.
+// console.log(newFigures[i]): Outputs the value of the element at index i of the array.
+    
