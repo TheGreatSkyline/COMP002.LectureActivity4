3f2ae98027a560ce5f2f6654985f6af2349be1a9
Joaquin Gomez <joaquingomez@my.whccd.edu>
2024-11-06 23:38:00 -0800

    made an array figures

--- js/script.js
+++ js/script.js
@@ -4,3 +4,4 @@
 *    Date: 22/10/2022
 *    Purpose: JavaScript for Lecture Activity 4
 */
+let figures = [25, 35, 36, 40, 42];//Array in JavaScript are zero index, meaning the first element is at index 0.
