fab27686ccb364bd975368286a92bfe5c9e089a9
Joaquin Gomez <joaquingomez@my.whccd.edu>
2024-11-06 23:41:04 -0800

    made new array

--- js/script.js
+++ js/script.js
@@ -7,3 +7,6 @@
 let figures = [25, 35, 36, 40, 42];//Array in JavaScript are zero index, meaning the first element is at index 0.
 console.log(figures[0]);// figures[0] accesses the first element of the array, which is 25 in this case.
 console.log(figures[figures.length - 2]);// To get the second to last element of the array, we use the formula: arrayName[arrayName.length - 2].
+// Figure.length returns the number of elements in the array, which is 5 in this case.
+// Since the array is zero-indexed, the last element is at index length - 1, giving us 42
+let newFigures = [25, 35, 36, 40, 42];
